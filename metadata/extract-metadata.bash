e97bef28707107443a69e09d10ae15ad34215be2
Jack Watson <jpw2065@rit.edu>
2022-04-10 04:42:40 -0400

    Wget filename cleaner

--- metadata/extract-metadata.bash
+++ metadata/extract-metadata.bash
@@ -15,8 +15,43 @@ fi
 
 mkdir -p "$REPORT/website"
 
+if [ -d "tmp" ]
+then
+	rm -r "tmp"
+fi
+
+mkdir "tmp"
+
 # Read url-sites.txt, download and save the file, then extract metadata and store in .csv. 
-cat $INPUT_URLS | while read line; do
-	wget -P $WEBSITE_DIRECTORY $line
-       echo $line	
+cat $INPUT_URLS | while read url; do
+
+	# Cleant the filename so we can better extract the metadata.
+	FILENAME=$(basename $url | grep -Po "[^=\n'\s]*\.[^?\n'\s]*" | head -1| sed -e "s/php/html/g")
+	DIRNAME=$(dirname $url | grep -Po "(?<!\/\/)(?<=\/)(?!\/).*")
+
+	if [ "$FILENAME" == "" ]
+	then
+		FILENAME="/$(basename $url).html"
+	else
+		FILENAME="/$FILENAME"
+	fi	
+
+	if [ "$DIRNAME" != "" ]
+	then
+		DIRNAME="/$DIRNAME"
+	fi
+
+	LOCALPATH="$WEBSITE_DIRECTORY$DIRNAME$FILENAME"
+
+	# Use base name to get the end of the file
+	wget -nd -O "tmp/downloaded_item" $url &> /dev/null
+
+	# Clean the filename so we can better extract the metadata.
+	if [ ! -d "tmp/downloaded_item" ]
+	then
+		echo "$LOCALPATH"
+		echo "------"
+		mkdir -p "$WEBSITE_DIRECTORY$DIRNAME"
+		mv tmp/downloaded_item $LOCALPATH
+	fi
 done
